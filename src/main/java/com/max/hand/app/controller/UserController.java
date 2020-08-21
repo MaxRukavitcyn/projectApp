1407c99789666a46adfde14b05aba91156070a9d
MaxHand <rukav77@gmail.com>
2020-08-22 00:19:22 +0500

    start

--- src/main/java/com/max/hand/app/controller/UserController.java
+++ src/main/java/com/max/hand/app/controller/UserController.java
@@ -6,10 +6,12 @@ import com.max.hand.app.service.SecurityService;
 import com.max.hand.app.service.UserService;
 import com.max.hand.app.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/")
+@Controller
 public class UserController {
 
     @Autowired
@@ -21,42 +23,48 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration(@RequestBody User userForm, BindingResult bindingResult) {
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
         userValidator.validate(userForm, bindingResult);
 
         if (bindingResult.hasErrors()) {
-            return "error";
+            return "registration";
         }
 
         userService.save(userForm);
 
         securityService.autoLogin(userForm.getUsername(), userForm.getConfirmPassword());
 
-        return "welcome";
+        return "redirect:/welcome";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(String error, String logout) {
+    public String login(Model model, String error, String logout) {
         if (error != null) {
-            return "Username or password is incorrect.";
+            model.addAttribute("error", "Username or password is incorrect.");
         }
 
         if (logout != null) {
-           return "Logged out successfully.";
+            model.addAttribute("message", "Logged out successfully.");
         }
 
         return "login";
     }
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
-    public String welcome() {
+    public String welcome(Model model) {
         return "welcome";
     }
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String admin() {
+    public String admin(Model model) {
         return "admin";
     }
 }
